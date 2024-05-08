d896a5a36e8f0b8e5a51b4c9ca44595ba5432d65
tsubokura-shuhei <repeat2639@gmail.com>
2024-05-08 23:45:01 +0900

    firebase fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 const firebaseSettings = require("./.");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4f5Ly10-nBSprjKOJcxkfRtigUGAWYFo",
-  authDomain: "react-household-account-app.firebaseapp.com",
-  projectId: "react-household-account-app",
-  storageBucket: "react-household-account-app.appspot.com",
-  messagingSenderId: "304188791015",
-  appId: "1:304188791015:web:93d5b6e5fe522e33262a42",
+  apiKey: "AIzaSyARgG3-Hza0cppG-wG6Uup7Yir7IYRXVE4",
+  authDomain: "dev-household.firebaseapp.com",
+  projectId: "dev-household",
+  storageBucket: "dev-household.appspot.com",
+  messagingSenderId: "501994246784",
+  appId: "1:501994246784:web:baae9be208ff1e948d8c65",
 };
 
 // Initialize Firebase
