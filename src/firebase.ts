9a997177a1dc2acee1caf2956a3e6e524f91973e
tsubokura-shuhei <repeat2639@gmail.com>
2024-05-11 23:32:59 +0900

    env add

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 const firebaseSettings = require("./.");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARgG3-Hza0cppG-wG6Uup7Yir7IYRXVE4",
-  authDomain: "dev-household.firebaseapp.com",
-  projectId: "dev-household",
-  storageBucket: "dev-household.appspot.com",
-  messagingSenderId: "501994246784",
-  appId: "1:501994246784:web:baae9be208ff1e948d8c65",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
